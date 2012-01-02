016898e30045d3ed007b12e363750aaac7d62d7e
Josh Finnie <joshua.finnie@gmail.com>
2012-01-02 13:42:01 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an example of a Job Board build using Flask, MongoDB, and Heroku.
---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from . import settings
+app = Flask(__name__)
+app.config.from_object(settings)
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
