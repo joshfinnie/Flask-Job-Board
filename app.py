b2846389d5d81d749c2e0a2e0f7a3e41b5533afd
Josh Finnie <joshua.finnie@gmail.com>
2012-01-03 14:43:04 -0500

    adding csrf protection

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, abort
 import settings
 
 from mongoengine import connect, Document, StringField, EmailField, BooleanField, DateTimeField, URLField
@@ -28,6 +28,20 @@ class Job(Document):
 	application_instructions = StringField(required=True)
 	telework = BooleanField(required=True)
 	created = DateTimeField(default = datetime.utcnow())
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = os.urandom(15)
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 @app.route("/")
 def hello():
