9587fb971333468f1989cf57ac2fd7bdfb11be9e
Josh Finnie <joshua.finnie@gmail.com>
2012-01-06 10:05:39 -0500

    fixed login wrapper

--- app.py
+++ app.py
@@ -54,10 +54,11 @@ class Job(Document):
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.username is None:
+        if session.logged_in:
+        	return f(*args, **kwargs)
+        else:
 			flash(u'Login is required.', 'warning')
 			return redirect(url_for('login', next=request.url))
-        return f(*args, **kwargs)
     return decorated_function
 
 @app.route("/")
